bc33d9320d855f43e63c16f422754390e9b94470
Kiran Thomas <thomas.kiran@heb.com>
2020-08-23 11:59:08 -0500

    chore: add build folder to git

---
+++ ui/build/precache-manifest.136dab19480110050dacfd295481f866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc679f6bc57acee58980e15e97e8fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f9f78636ac24245e47",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d12cd0bca012ca01d08",
+    "url": "/static/js/2.31d7ad62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31d7ad62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f9f78636ac24245e47",
+    "url": "/static/js/main.13895b49.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
