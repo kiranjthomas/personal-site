08bde7bb8556b0784adc9978701d28ff22cbbbfc
Kiran Thomas <thomas.kiran@heb.com>
2020-08-27 16:52:19 -0500

    feat: refactor github actions

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Kiran Thomas!</div>
+        <div>Kiran Thomas!!</div>
       </header>
     </div>
   );
